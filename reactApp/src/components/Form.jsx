3f99467f32da963cc0c96fe756e852b89098fee7
drew <drewtuannguyen2003@gmail.com>
2024-11-30 17:52:01 -0500

    form changes to signup page

--- reactApp/src/components/Form.jsx
+++ reactApp/src/components/Form.jsx
@@ -8,13 +8,18 @@ export default function Form() {
   return (
     <form>
       <div id="notification"></div>
+      <div>
         <FormComponent labeltext="Email:" typeOfInput = "email" name="youremail"/>
-        <FormComponent labeltext="Password (at least 6 characters long):" typeOfInput="password" name="yourpassword"/>
-        <div className="form-buttons">
-          <ButtonNavLink to="/" text="<= Back" id="goToLoginPageButton"/>
-            <SubmitButton id="submitButton1" text="submit"/>
-          <ButtonNavLink to="/login" text="Next =>" id="goToLoginPageButton"/>
-        </div>
+      </div>
+      <div>
+        <FormComponent labeltext="Password:" typeOfInput="password" name="yourpassword"/>
+      </div>
+      <div className="form-buttons">
+        <ButtonNavLink to="/" text="<= Back" id="goBackToHomePageButton"/>
+        <SubmitButton id="submitButton1" text="submit"/>
+        <ButtonNavLink to="/login" text="Next =>" id="goToLoginPageButton"/>
+      </div>
+
       <div>
         <p>Already have an account? Click here to <NavLink to="/login">Login</NavLink></p> 
       </div>
--- reactApp/src/components/FormComponent.jsx
+++ reactApp/src/components/FormComponent.jsx
@@ -1,10 +1,15 @@
 import Label from "./Label"
+import "./FormComponent.css"
 /* eslint-disable react/prop-types */
 export default function FormComponent({labeltext, typeOfInput, name}) {
   return (
     <div className="form-group">
-      <Label text={labeltext}/>
+      <div>
+        <Label text={labeltext}/>
+      </div>
+      <div>
       <input type={typeOfInput} name={name} />
+      </div>
   </div>
   )
 }
